2dcb9c31cf5e7a9dd0ac4ea26494ec6b25f7cd3a
CipiVlad <supercoder.cipi@gmail.com>
2022-04-19 16:26:31 +0200

    b-day-calc

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+let inputNumber = document.getElementById('geburtsjahr');
+
+function showGeburtsjahr() {
+    // console.log('works');
+    // console.log(2022 - inputNumber.value);
+    output.innerHTML = 2022 - inputNumber.value;
+};
+// showGeburtsjahr();
